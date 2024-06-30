8ec4e51ac535b02feb36fd4be61d1baa6f9868ec
sint18 <htoohtoo.slh@gmail.com>
2024-06-30 13:25:06 +0100

    added readme.md

---
+++ Readme.md
@@ -0,0 +1,35 @@
+# Maze Generator Web
+
+This is a web based maze generator using HTML, CSS and Vanilla JavaScript.
+
+## ⚙️Algorithm
+
+Depth-first search or recursive backtracking algorithm is used to generate mazes.
+In this demo, both recursive function and iterative function (stack) are used.
+
+## 🚀Running
+
+### 1. Node.js
+
+> Node.js is required
+
+Run the following command to run a live server.
+A default browser will automatically open.
+
+`npx live-server`
+
+### 2. JetBrainsIDE
+
+If you use Phpstorm or something similar.
+1. Right click on 'index.html'.
+2. Choose 'Open in' > 'Browser' > 'Default'.
+
+### 3. VSCode
+
+If you use VSCode, do the following.
+1. Install **Live Server** extension.
+2. Right click on 'index.html' and click on 'Open with Live Server'.
+
+## 🧑‍💻Contact
+
+Sint Lwin Htoo - sintlwinhtoo.slh@gmail.com
--- js/app.js
+++ js/app.js
@@ -1,30 +1,20 @@
-import { createMaze } from "./mazeGenerator.mjs";
+import {createMaze} from "./mazeGenerator.mjs";
 
 const canvas = document.getElementById('mazeCanvas')
 const ctx = canvas.getContext("2d");
 
 const sizeInput = document.getElementById('sizeInput')
 const generateBtn = document.getElementById('generateBtn')
-const resetBtn = document.getElementById('resetBtn')
-const messageDiv= document.getElementById('message')
+const messageDiv = document.getElementById('message')
 generateBtn.addEventListener('click', () => {
+  messageDiv.textContent = ''
   const size = Number(sizeInput.value)
-  if (size > 0 && size < 1000) {
-    try {
-      createMaze(size)
-    } catch ({name, message}) {
-      messageDiv.textContent = name + ': ' + message
-    }
-    generateBtn.setAttribute('disabled', null)
-    sizeInput.setAttribute('readonly', null)
+  try {
+    if (size <= 0) throw { name: 'Too Small', message: 'Try a bigger number'}
+    createMaze(size)
+  } catch ({name, message}) {
+    messageDiv.textContent = name + ': ' + message
   }
-})
-resetBtn.addEventListener('click', () => {
-  ctx.reset()
-  generateBtn.removeAttribute('disabled')
-  sizeInput.removeAttribute('readonly')
-
-
 })
 
 
